6a9ed2cd48d993343249693bbebad4ab54284506
Kuldeep Rana <kuldeep.rana@puresoftware.com>
2022-01-08 10:10:02 +0530

    deleted old projects

--- orderservice/src/main/java/com/codersdesks/orderservice/repository/OrderRepository.java
+++
@@ -1,8 +0,0 @@
-package com.codersdesks.orderservice.repository;
-
-import com.codersdesks.orderservice.entity.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order,Integer> {
-
-}
--- paymentservice/src/main/java/com/codersdesks/paymentservice/repository/PaymentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.codersdesks.paymentservice.repository;
-
-import com.codersdesks.paymentservice.entity.Payment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PaymentRepository extends JpaRepository<Payment,Integer> {
-
-}
