24719164e585dd78c295b6abab5ede9e6d70a690
Kuldeep Rana <kuldeep.rana@puresoftware.com>
2022-01-11 23:01:21 +0530

    added discovery server

--- movie-catalog-service/src/main/java/com/codersdesks/MovieCatalogServiceApplication.java
+++ movie-catalog-service/src/main/java/com/codersdesks/MovieCatalogServiceApplication.java
@@ -5,12 +5,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MovieCatalogServiceApplication {
 
 	public static void main(String[] args) {
@@ -23,6 +26,7 @@ public class MovieCatalogServiceApplication {
 	}
 
 	@Bean
+	@LoadBalanced
 	public WebClient.Builder getWebClient(){
 		return  WebClient.builder();
 	}
--- movie-discovery-server/src/main/java/com/codersdesks/MovieDiscoveryServerApplication.java
+++ movie-discovery-server/src/main/java/com/codersdesks/MovieDiscoveryServerApplication.java
@@ -2,14 +2,14 @@ package com.codersdesks;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaClient
-public class MovieInfoServiceApplication {
+@EnableEurekaServer
+public class MovieDiscoveryServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MovieInfoServiceApplication.class, args);
+		SpringApplication.run(MovieDiscoveryServerApplication.class, args);
 	}
 
 }
